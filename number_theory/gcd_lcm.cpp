83a2dfe5a890aae91d1fa0eefc8bd5e885c58f2a
Roshni Shukla <71212064+Roshni1809@users.noreply.github.com>
2021-03-01 00:21:38 +0530

    Find the GCD and LCM of two numbers

---
+++ number_theory/gcd_lcm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cin >> num1 >> num2;
+    
+    int n1=num1;
+    int n2=num2;
+    
+    while(num1%num2!=0)
+    {
+        int rem=num1%num2;
+        num1=num2;
+        num2=rem;
+    }
+    
+    int gcd= num2;
+    
+    int lcm=(n1*n2)/gcd;
+
+    cout<<gcd<<endl<<lcm;
+    
+    return 0;
+    
+}
